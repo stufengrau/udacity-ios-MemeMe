275b04431fab86e26df979d82157f967001cb475
stufengrau <stufengrau@gmail.com>
2016-10-17 22:51:10 +0200

    Remove unnecessary template functions

--- MemeMe/MemeCollectionViewController.swift
+++ MemeMe/MemeCollectionViewController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-//private let reuseIdentifier = "Cell"
-
 var memes: [Meme] {
     return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
 }
@@ -37,15 +35,6 @@ class MemeCollectionViewController: UICollectionViewController {
         collectionView?.reloadData()
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: UICollectionViewDataSource
     
@@ -68,38 +57,5 @@ class MemeCollectionViewController: UICollectionViewController {
         detailVC.meme = memes[indexPath.row]
         self.navigationController?.pushViewController(detailVC, animated: true)
     }
-
-
-
-    // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
-    
-    }
-    */
 
 }
--- MemeMe/MemeDetailViewController.swift
+++ MemeMe/MemeDetailViewController.swift
@@ -29,14 +29,4 @@ class MemeDetailViewController: UIViewController {
         tabBarController?.tabBar.hidden = false
     }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
